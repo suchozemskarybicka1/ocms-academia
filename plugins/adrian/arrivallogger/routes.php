f80043c0eba7170dbd5a602ecdbc230194d89cbf
Adrian Dvorsky <dvorsky@wezeo.com>
2022-10-11 19:54:23 +0200

    skusanie eloquent ORM

--- plugins/adrian/arrivallogger/routes.php
+++ plugins/adrian/arrivallogger/routes.php
@@ -19,9 +19,26 @@ Route::post('add-arrival', function () {
 });
 
 
-Route::get('orm', function () {
+Route::get('orm1', function () {
 
     $log = Arrivallogger::where('name', 'LIKE', 'Jozo')->get();
+    return $log;
+
+});
+
+Route::get('orm2', function () {
+
+    $log = Arrivallogger::where('name', 'LIKE', 'Jozo')
+        ->orWhere('late', 'LIKE', 1)
+        ->get();
+    return $log;
+
+});
+
+
+Route::get('orm3', function () {
+
+    $log = Arrivallogger::orderBy('created_at', 'desc')->get();
     return $log;
 
 });
